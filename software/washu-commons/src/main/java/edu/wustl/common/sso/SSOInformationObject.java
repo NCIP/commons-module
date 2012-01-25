025570d1b83623fd48e72bed9bed1afb158e025d
marwahan <marwahan>
2012-01-25 08:27:06 +0000

    Changes for SSO related feature.
    SVN-Revision: 63

---
+++ software/washu-commons/src/main/java/edu/wustl/common/sso/SSOInformationObject.java
@@ -0,0 +1,60 @@
+package edu.wustl.common.sso;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMessages;
+
+public class SSOInformationObject {
+	
+	private String loginName;
+	
+	private HttpServletRequest request;
+	
+	private ActionMessages actionMessages;
+	
+	private ActionErrors actionErrors;
+	
+	private String forwardTo;
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public ActionMessages getActionMessages() {
+		return actionMessages;
+	}
+
+	public void setActionMessages(ActionMessages actionMessages) {
+		this.actionMessages = actionMessages;
+	}
+
+	public ActionErrors getActionErrors() {
+		return actionErrors;
+	}
+
+	public void setActionErrors(ActionErrors actionErrors) {
+		this.actionErrors = actionErrors;
+	}
+
+	public String getForwardTo() {
+		return forwardTo;
+	}
+
+	public void setForwardTo(String forwardTo) {
+		this.forwardTo = forwardTo;
+	}
+
+}
